a279f270d4b72ed9935742c0a9fa4d672afc9c60
gns0170 <gns0170@gmail>
2022-03-15 10:27:29 +0900

    3/15 views singlechildScrollview

--- lib/widgets/view.dart
+++ lib/widgets/view.dart
@@ -3,10 +3,13 @@ import '../values/colors.dart' as custom_color;
 
 Widget views(inform, context, width) {
   return Container(
-    padding: const EdgeInsets.all(8.0),
-    width: MediaQuery.of(context).size.width - 40,
-    height: width.toDouble() ?? 300,
-    color: custom_color.primaryColor1.withOpacity(0.8),
-    child: inform ?? inform,
-  );
+      padding: const EdgeInsets.all(8.0),
+      width: MediaQuery.of(context).size.width - 40,
+      height: width.toDouble() ?? 300,
+      color: custom_color.primaryColor1.withOpacity(0.8),
+      child: SingleChildScrollView(
+        child: Container(
+          child: inform ?? inform,
+        ),
+      ));
 }
