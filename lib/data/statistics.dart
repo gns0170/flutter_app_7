5ce6424709ef5cfb03d3ee3f52ba505ea4120c25
gns0170 <gns0170@gmail>
2022-06-24 18:10:24 +0900

    6/24, statistic data type defined

---
+++ lib/data/statistics.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Statistic {
+  final String key;
+  final String title;
+  final int value;
+
+  Statistic(this.key, this.title, this.value);
+}
+
+List<Statistic> s = [];
